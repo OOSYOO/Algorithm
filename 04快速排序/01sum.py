a5f0a10999b095409149e474696824eda7adc163
yue.shen <381940186@qq.com>
2020-04-21 08:22:05 +0800

    init

--- 04快速排序/01sum.py
+++ 04快速排序/01sum.py
@@ -9,6 +9,12 @@ def lenArr(arr):
 
     return 1+lenArr(arr[1:])
 
+def maxNum(arr):
+    if(len(arr) <= 1):
+        return arr[0]
+
+    return max(arr[0], maxNum(arr[1:]))
+    
 if __name__ == "__main__":
     arr = [1,2,3,4,5]
     print(sum(arr))
