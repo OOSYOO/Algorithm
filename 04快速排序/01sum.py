117456fcf08e163ba1d78cd23c08df9d02ae2632
yue.shen <381940186@qq.com>
2020-04-21 08:21:06 +0800

    init

---
+++ 04快速排序/01sum.py
@@ -0,0 +1,15 @@
+def sum(arr):
+    if len(arr) <= 1:
+        return arr[0]
+    return arr[0] + sum(arr[1:])
+
+def lenArr(arr):
+    if(len(arr) <=1 ):
+        return 1
+
+    return 1+lenArr(arr[1:])
+
+if __name__ == "__main__":
+    arr = [1,2,3,4,5]
+    print(sum(arr))
+    print(lenArr(arr))
